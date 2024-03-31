209b22557c2356a1cd36f4d236bd04f4888802e8
dexnis8 <dexnis30@gmail.com>
2024-03-31 16:48:20 -0700

    added score

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,12 +3,64 @@
 // Question.js
 import { useEffect, useState } from "react";
 
+// const Question = ({
+//   question,
+//   onAnswerCheck,
+//   onNextQuestion,
+//   attemptsLeft,
+//   errorMessage,
+// }) => {
+//   const [userAnswer, setUserAnswer] = useState("");
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     onAnswerCheck(userAnswer);
+//   };
+
+//   useEffect(() => {
+//     setUserAnswer("");
+//   }, [question]);
+
+//   return (
+//     <div className="max-w-md mx-auto my-8 p-6 bg-gray-100 rounded-lg shadow-md">
+//       <h2 className="text-xl font-semibold mb-4">{question.text}</h2>
+//       {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           className="w-full px-4 py-2 mb-4 border rounded-md"
+//           type="text"
+//           value={userAnswer}
+//           onChange={(e) => setUserAnswer(e.target.value)}
+//           placeholder={question.placeholder}
+//         />
+//         <button
+//           className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
+//           type="submit"
+//         >
+//           Check Answer
+//         </button>
+//         {/* <button
+//           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+//           type="button"
+//           onClick={onNextQuestion}
+//         >
+//           Next
+//         </button> */}
+//       </form>
+//       <p className="text-gray-500">Attempts left: {attemptsLeft}</p>
+//     </div>
+//   );
+// };
+
 const Question = ({
   question,
   onAnswerCheck,
   onNextQuestion,
   attemptsLeft,
   errorMessage,
+  totalQuestions,
+  currentQuestion,
+  score,
 }) => {
   const [userAnswer, setUserAnswer] = useState("");
 
@@ -17,13 +69,13 @@ const Question = ({
     onAnswerCheck(userAnswer);
   };
 
-  useEffect(() => {
-    setUserAnswer("");
-  }, [question]);
-
   return (
     <div className="max-w-md mx-auto my-8 p-6 bg-gray-100 rounded-lg shadow-md">
+      <h2 className="text-xl font-semibold mb-4">
+        Question {currentQuestion}/{totalQuestions}
+      </h2>
       <h2 className="text-xl font-semibold mb-4">{question.text}</h2>
+      <p className="mb-2">Score: {score}</p>
       {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
       <form onSubmit={handleSubmit}>
         <input
@@ -31,7 +83,7 @@ const Question = ({
           type="text"
           value={userAnswer}
           onChange={(e) => setUserAnswer(e.target.value)}
-          placeholder={question.placeholder}
+          placeholder="Type your answer..."
         />
         <button
           className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
@@ -39,13 +91,13 @@ const Question = ({
         >
           Check Answer
         </button>
-        {/* <button
+        <button
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
           type="button"
           onClick={onNextQuestion}
         >
           Next
-        </button> */}
+        </button>
       </form>
       <p className="text-gray-500">Attempts left: {attemptsLeft}</p>
     </div>
